3211b6497b0dac404845bac183ae75bacb81471c
Deepak Gola <er.deepakgola@gmail.com>
2025-01-05 19:37:50 +0530

    Enhancing documentation for Application header details such as the application description, whom to contact(contact details), and license, and external documentation, wiki links

--- src/main/java/com/simplebank/accounts/AccountsApplication.java
+++ src/main/java/com/simplebank/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.simplebank.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservice",
+				version = "1.0",
+				description = "Accounts Microservice API",
+				contact = @Contact(
+						name = "Deepak Gola",
+						email = "er.deepakgola@gmail.com",
+						url = "raudria.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "http://www.apache.org/licenses/LICENSE-2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Accounts Microservice Wiki",
+				url = "raudria.com/simplebank/docs"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
